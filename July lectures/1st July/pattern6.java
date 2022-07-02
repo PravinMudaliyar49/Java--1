d9b1b3255092f2570a1a6b6af34c44e3fe168982
Pravin <pravincoding49@gmail.com>
2022-07-02 20:27:02 +0530

    2ns July

--- July lectures/1st July/pattern6.java
+++ July lectures/1st July/pattern6.java
@@ -1,9 +1,7 @@
 public class pattern6 {
     public static void main(String[] args) {
         int n = 6;
-
-        being_exception_
-
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 if (i == j) {
