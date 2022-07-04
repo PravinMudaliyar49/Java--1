7b2df34c7d1b57a5bf6a566c99d1e8458afbf5fa
Pravin <pravincoding49@gmail.com>
2022-07-04 21:59:04 +0530

    4th July

---
+++ July lectures/4th July/binarySystem.java
@@ -0,0 +1,12 @@
+public class binarySystem {
+    public static void main(String[] args) {
+        byte b = 127;
+        System.out.println(b);
+
+        int x = Integer.MAX_VALUE;
+        System.out.println(x);
+
+        int y = Integer.MIN_VALUE;
+        System.out.println(y);
+    }
+}
