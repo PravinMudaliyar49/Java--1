a14827d80f1c33677ee05bd57da8713ce826e57b
Pravin <pravincoding49@gmail.com>
2022-07-07 22:04:02 +0530

    7th July

--- July lectures/7th July/pattern12.java
+++ July lectures/7th July/pattern12.java
@@ -2,6 +2,23 @@ import java.util.*;
 
 public class pattern12 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int a = 0, b = 1, c = 1;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                c = a + b;
+                System.out.print(b + "\t");
+
+                a = b;
+                b = c;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
