8f167c10c9125f969f889e176d130f1d49021646
Pravin <pravincoding49@gmail.com>
2022-07-18 22:05:25 +0530

    18th July

---
+++ July lectures/18th July/coverup.java
@@ -0,0 +1,25 @@
+
+public class coverup {
+    public static void main(String[] args) {
+        // char ch = 'a';
+
+        // int y = ch;
+
+        // int y = 10;
+
+        // char x = y;
+
+        // int x = 13;
+
+        // byte b = (byte) x;      // cast operator.
+
+        // System.out.println(b);
+
+        long l = 10;
+
+        int x = (int) l;
+
+        System.out.println(x);
+
+    }
+}
